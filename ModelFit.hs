64bd37afbe2a4e387303fe998fabca2336d68a1f
Ben Gamari <bgamari.foss@gmail.com>
2013-03-22 22:52:47 -0400

    Work

--- ModelFit.hs
+++ ModelFit.hs
@@ -20,6 +20,7 @@ import           Data.Colour.Names
 import           Graphics.Rendering.Chart
 import           System.Environment
 
+-- | Diffusion model
 data FcsModel a = Fcs { fcsTauD     :: a  -- ^ diffusion time (us)
                       , fcsA        :: a  -- ^ aspect ratio
                       , fcsG0       :: a  -- ^ amplitude
@@ -37,6 +38,69 @@ instance Additive FcsModel where
 
 instance Metric FcsModel
 
+fcsCorr :: (RealFloat a) => FcsModel a -> a -> a
+fcsCorr (Fcs {..}) tau =
+    fcsG0 / (1 + ttd) / sqrt (1 + 1/(fcsA*fcsA) * ttd) + fcsGinf
+  where ttd = tau / fcsTauD
+
+
+
+-- | Diffusion model with triplet
+data FcsTriplet a = FcsT { fcsF    :: a
+                         , fcsTauF :: a
+                         , fcsFcs  :: FcsModel a
+                         }
+                  deriving (Show, Eq, Functor, Foldable, Traversable)
+
+instance Applicative FcsTriplet where
+    pure a = FcsT a a (pure a)
+    FcsT a b c <*> FcsT a' b' c'  = FcsT (a a') (b b') (c <*> c')
+
+instance Additive FcsTriplet where
+    zero = pure 0
+
+instance Metric FcsTriplet
+
+fcsTripletCorr :: (RealFloat a) => FcsTriplet a -> a -> a
+fcsTripletCorr (FcsT {..}) tau =
+    (1 - fcsF + fcsF * exp (-tau / fcsTauF)) / (1 - fcsF) * fcsCorr fcsFcs tau
+
+
+-- | Sum model
+data SumM f g a = SumM (f a) (g a)
+                deriving (Show, Eq, Functor, Foldable, Traversable)
+
+instance (Applicative f, Applicative g) => Applicative (SumM f g) where
+    pure a = SumM (pure a) (pure a)
+    SumM f g <*> SumM f' g' = SumM (f <*> f') (g <*> g')
+
+instance (Additive f, Applicative f, Additive g, Applicative g) => Additive (SumM f g) where
+    zero = pure 0
+
+instance (Metric f, Foldable f, Applicative f, Metric g, Foldable g, Applicative g) =>
+         Metric (SumM f g)
+
+sumM :: (Num a) => (f a -> a) -> (g a -> a) -> SumM f g a -> a
+sumM f g (SumM fa ga) = f fa + g ga
+
+-- | Product model
+data ProdM f g a = ProdM (f a) (g a)
+                deriving (Show, Eq, Functor, Foldable, Traversable)
+
+instance (Applicative f, Applicative g) => Applicative (ProdM f g) where
+    pure a = SumM (pure a) (pure a)
+    SumM f g <*> SumM f' g' = SumM (f <*> f') (g <*> g')
+
+instance (Additive f, Applicative f, Additive g, Applicative g) => Additive (ProdM f g) where
+    zero = pure 0
+
+instance (Metric f, Foldable f, Applicative f, Metric g, Foldable g, Applicative g) =>
+         Metric (ProdM f g)
+
+prodM :: (Num a) => (f a -> a) -> (g a -> a) -> ProdM f g a -> a
+prodM f g (SumM fa ga) = f fa * g ga
+
+-- | Other
 data Scope a = Fixed a
              | Fitted
              deriving (Show)
@@ -47,27 +111,32 @@ withScope s x = f <$> s <*> x
         f (Fixed x) _ = x
         f Fitted    x = x
 
-fcsCorr :: (RealFloat a) => FcsModel a -> a -> a
-fcsCorr (Fcs {..}) tau =
-    fcsG0 / (1 + ttd) / sqrt (1 + 1/(fcsA*fcsA) * ttd) + fcsGinf
-  where ttd = tau / fcsTauD
-
 sumSqResidual :: (Functor f, Foldable f, RealFrac a) => (a -> a) -> f (Obs a) -> a
 sumSqResidual f = Foldable.sum . fmap (\(Obs x y s)->(f x - y)^2 / s)
 
 main = do
     fname:_ <- getArgs
     Right corr' <- readCorr fname
-    let corr = V.map (\o->o {oX=1e6*oX o}) $ V.filter (\o->oX o > 1e-6) corr'
+    let corr = V.map (\o->o {oX=1e6*oX o})
+               -- $ V.filter (\o->oX o > 1e-6)
+               corr'
     let p0 = Fcs { fcsTauD  = 100
                  , fcsA     = 10
                  , fcsG0    = 10
                  , fcsGinf  = 1
                  , fcsAlpha = 1
                  }
-    let fit :: RealFloat a => FcsModel a -> a
-        fit p = sumSqResidual (fcsCorr p) $ fmap (fmap realToFrac) corr
-        dfit :: RealFloat a => FcsModel a -> FcsModel a
+        t0 = FcsT { fcsF    = 0.1
+                  , fcsTauF = 0.1
+                  , fcsFcs = p0
+                  }
+    --let fit :: RealFloat a => FcsModel a -> a
+    --    fit p = sumSqResidual (fcsCorr p) $ fmap (fmap realToFrac) corr
+    --    dfit :: RealFloat a => FcsModel a -> FcsModel a
+    --    dfit = grad fit
+    let fit :: RealFloat a => FcsTriplet a -> a
+        fit p = sumSqResidual (fcsTripletCorr p) $ fmap (fmap realToFrac) corr
+        dfit :: RealFloat a => FcsTriplet a -> FcsTriplet a
         dfit = grad fit
 
     let go :: (a -> String) -> Int -> [a] -> IO a
@@ -77,10 +146,10 @@ main = do
 
     let search = backtrackingSearch 0.1 0.2
         beta = fletcherReeves
-    p1 <- go (\p->show (sumSqResidual (fcsCorr (fmap realToFrac p)) corr, p)) 10000
-             $ conjGrad search beta fit dfit p0
+    p1 <- go (\p->show (sumSqResidual (fcsTripletCorr (fmap realToFrac p)) corr, p)) 1000
+             $ conjGrad search beta fit dfit t0
     print p1
-    renderableToSVGFile (toRenderable $ plotFit corr [fcsCorr p1]) 800 800 "out.svg"
+    renderableToSVGFile (toRenderable $ plotFit corr [fcsTripletCorr p1]) 800 800 "out.svg"
 
 data Obs a = Obs { oX :: !a   -- ^ Abscissa
                  , oY :: !a   -- ^ Ordinate
