f43e3cee60e77903193ec1f398713affbbb739fc
Ben Gamari <bgamari.foss@gmail.com>
2013-03-24 11:20:40 -0400

    Implement penalty method

--- Constrained.hs
+++ Constrained.hs
@@ -55,31 +55,40 @@ constrainGT :: (Num a) => (forall s. Mode s => f (AD s a) -> AD s a)
 constrainGT h (Opt f gs hs) = Opt f gs (augment (FU $ negate . h) hs)
 
 -- | Minimize the given constrained optimization problem
-minimize :: (Functor f, Num a, g ~ V)
+-- This is a basic penalty method approach
+minimize :: (Functor f, Num a, Ord a, g ~ V)
          => (FU f a -> f a -> [f a])   -- ^ Primal minimizer
-         -> (FU g a -> g a -> [g a])   -- ^ Dual minimizer
          -> Opt f a                    -- ^ The optimization problem of interest
+         -> a                          -- ^ The penalty increase factor
          -> f a                        -- ^ The primal starting value
          -> g a                        -- ^ The dual starting value
          -> [f a]                      -- ^ Optimizing iterates
-minimize minX minL opt = go
-  where go x0 l0 = let l1 = head $ drop 100 $ minL (FU $ \l -> -lagrangian opt (fmap auto x0) l) l0
-                       x1 = head $ drop 100 $ minX (FU $ \x ->  lagrangian opt x (fmap auto l1)) x0
+minimize minX opt alpha = go
+  where go x0 l0 = let l1 = fmap (*alpha) l0
+                       x1 = head $ drop 100 $ minX (FU $ \x -> augLagrangian opt x (fmap auto l1)) x0
                    in x1 : go x1 l1
 
 -- | Maximize the given constrained optimization problem
-maximize :: (Functor f, Num a, g ~ V)
+maximize :: (Functor f, Num a, Ord a, g ~ V)
          => (FU f a -> f a -> [f a])   -- ^ Primal minimizer
-         -> (FU g a -> g a -> [g a])   -- ^ Dual minimizer
          -> Opt f a                    -- ^ The optimization problem of interest
+         -> a                          -- ^ The penalty increase factor
          -> f a                        -- ^ The primal starting value
          -> g a                        -- ^ The dual starting value
          -> [f a]                      -- ^ Optimizing iterates
-maximize minX minL (Opt (FU f) gs hs) =
-    minimize minX minL (Opt (FU $ negate . f) gs hs)
+maximize minX (Opt (FU f) gs hs) alpha =
+    minimize minX (Opt (FU $ negate . f) gs hs) alpha
 
 -- | The Lagrangian for the given constrained optimization
 lagrangian :: (Num a) => Opt f a
            -> (forall s. Mode s => f (AD s a) -> V (AD s a) -> AD s a)
 lagrangian (Opt (FU f) gs hs) x l =
     f x - V.sum (V.zipWith (\lamb (FU g)->lamb * g x) l gs)
+
+-- | The augmented Lagrangian for the given constrained optimization
+augLagrangian :: (Num a, Ord a) => Opt f a
+           -> (forall s. Mode s => f (AD s a) -> V (AD s a) -> AD s a)
+augLagrangian (Opt (FU f) gs hs) x l =
+    f x + V.sum (V.zipWith (*) l $ V.concat [gs', hs'])
+  where gs' = V.map (\(FU g) -> (g x)^2) gs
+        hs' = V.map (\(FU h) -> (max 0 $ h x)^2) hs
--- Test.hs
+++ Test.hs
@@ -15,14 +15,8 @@ instance Metric V.Vector
 
 xmin :: (Show a, Show (f a), Additive f, Metric f, Traversable f, RealFloat a)
      => FU f a -> f a -> [f a]
-xmin (FU f) = steepestDescent search (lowerFU f) df
-  where df = grad f
-        search = backtrackingSearch 1e-2 1e-3
-        --search = constantSearch 1e-4
-
-lmin :: (Show a, Show (f a), Additive f, Metric f, Traversable f, RealFloat a)
-     => FU f a -> f a -> [f a]
-lmin (FU f) = steepestDescent search (lowerFU f) df
+--xmin (FU f) = steepestDescent search (lowerFU f) df
+xmin (FU f) = conjGrad search fletcherReeves (lowerFU f) df
   where df = grad f
         search = backtrackingSearch 1e-2 1e-3
         --search = constantSearch 1e-4
@@ -30,13 +24,13 @@ lmin (FU f) = steepestDescent search (lowerFU f) df
 f (V2 x y) = 2 - x^2 - 2*y^2
 
 opt :: Opt V2 Double
-opt =   constrainEQ (\(V2 x y) -> x^2 + y^2 - 1)
+opt =   constrainGT (\(V2 x y) -> x^2 + y^2 - 1)
       $ optimize f
 
-x0 = V2 1 1
+x0 = V2 1 10
 l0 = V.replicate 1 1 :: V Double
 
 main = do
     let a :: [V2 Double]
-        a = maximize xmin lmin opt x0 l0
-    mapM_ print $ take 10000 a
+        a = maximize xmin opt 2 x0 l0
+    mapM_ print $ take 1000 a
