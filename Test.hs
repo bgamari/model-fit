45be7eb964a8e695f644cb6c31f7f5b45716a442
Ben Gamari <bgamari.foss@gmail.com>
2013-03-23 23:22:27 -0400

    Debugging work

--- Test.hs
+++ Test.hs
@@ -8,40 +8,37 @@ import Linear hiding (trace)
 import Numeric.AD
 import Numeric.AD.Types
 import Optimization.LineSearch.ConjugateGradient
+import Optimization.LineSearch.SteepestDescent
 import qualified Data.Vector as V
 
 import Debug.Trace
-import Unsafe.Coerce
-yes = unsafeCoerce  -- Until type inference on Nats works
 
 instance Metric V.Vector
 
---xmin :: (Show a, Show (f a), Additive f, Metric f, Traversable f, RealFloat a)
---     => FU f a -> f a -> [f a]
---xmin (FU f) = trace "adsf" . conjGrad search beta (const 1) df --(tr . lowerFU f) df
-xmin (FU f) = trace "adsf" $ df
-  where df = tr . grad f
+xmin :: (Show a, Show (f a), Additive f, Metric f, Traversable f, RealFloat a)
+     => FU f a -> f a -> [f a]
+xmin (FU f) = steepestDescent search (lowerFU f) df
+  where df = grad f
         search = backtrackingSearch 0.1 0.2
-        beta = fletcherReeves
 
 lmin :: (Show a, Show (f a), Additive f, Metric f, Traversable f, RealFloat a)
      => FU f a -> f a -> [f a]
-lmin (FU f) = tr . conjGrad search beta (lowerFU f) df
+lmin (FU f) = steepestDescent search (lowerFU f) df
   where df = grad f
         search = backtrackingSearch 0.1 0.2
-        beta = fletcherReeves
 
 tr x = traceShow x x
 
-opt :: Opt V2 Double
-opt = yes
-      $ constrainEQ (\(V2 x y) -> traceShow ('g',x,y) $ x^2 + y^2)
-      $ optimize (\(V2 x y) -> traceShow ('f',x,y) $ 2 - x^2 - 2*y^2)
+f (V2 x y) = 2 - x^2 - 2*y^2
 
-x0 = V2 0 0
+opt :: Opt V2 Double
+opt =   constrainEQ (\(V2 x y) -> traceShow ('g',x,y) $ x^2 + y^2 - 1)
+      $ optimize (\v@(V2 x y) -> traceShow ('f',x,y) $ f v)
+
+x0 = V2 1 1
 l0 = V.replicate 1 1 :: V Double
 
 main = do
     let a :: [V2 Double]
         a = minimize xmin lmin opt x0 l0
-    print $ head a
+    print $ take 3 a
